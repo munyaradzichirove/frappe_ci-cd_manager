bdeb8bfe33cc92c0bd5b5ae878e1fe0bf185f8be
munyaradzi <chirovemunyaradzi@gmail.com>
2026-02-18 17:40:29 +0200

    testfield

--- orchestrator/api.py
+++ orchestrator/api.py
@@ -5,46 +5,47 @@ import requests
 import subprocess
 import os
 
+
 @frappe.whitelist(allow_guest=True)
 def github_webhook(**kwargs):
-    # import json
-    # payload = kwargs.get("payload")
-    # if isinstance(payload, str):
-    #     payload = json.loads(payload)
+    import json
+    payload = kwargs.get("payload")
+    if isinstance(payload, str):
+        payload = json.loads(payload)
 
-    # commits = payload.get("commits", [])
-    # tz = timezone("Africa/Harare")
-    # repo_url = payload.get("repository", {}).get("html_url")
-    # if not repo_url:
-    #     frappe.throw("No repository URL found in payload")
-    # # get parent using repo filter
-    # doc_list = frappe.get_all("App Manager", filters={"repo": repo_url}, limit=1)
-    # if doc_list:
-    #     doc = frappe.get_doc("App Manager", doc_list[0].name)
-    # else:
-    #     # create parent if it doesn't exist
-    #     doc = frappe.get_doc({
-    #         "doctype": "App Manager",
-    #         "repo": repo_url
-    #     }).insert(ignore_permissions=True)
+    commits = payload.get("commits", [])
+    tz = timezone("Africa/Harare")
+    repo_url = payload.get("repository", {}).get("html_url")
+    if not repo_url:
+        frappe.throw("No repository URL found in payload")
+    # get parent using repo filter
+    doc_list = frappe.get_all("App Manager", filters={"repo": repo_url}, limit=1)
+    if doc_list:
+        doc = frappe.get_doc("App Manager", doc_list[0].name)
+    else:
+        # create parent if it doesn't exist
+        doc = frappe.get_doc({
+            "doctype": "App Manager",
+            "repo": repo_url
+        }).insert(ignore_permissions=True)
 
 
-    # for commit in commits:
-    #     committer = commit.get("committer", {}).get("name")
-    #     commit_id = commit.get("id")
-    #     message = commit.get("message")
-    #     received_time = now_datetime().astimezone(tz)
+    for commit in commits:
+        committer = commit.get("committer", {}).get("name")
+        commit_id = commit.get("id")
+        message = commit.get("message")
+        received_time = now_datetime().astimezone(tz)
 
-    #     doc.append("commit_history", {
-    #         "user": committer,
-    #         "commit_sha": commit_id,
-    #         "commit_message": message,
-    #         "received_time": received_time
-    #     })
-    # doc.save(ignore_permissions=True)
-    # send_telegram_message(committer, commit_id, message, received_time)
-    # run_ansible_playbook(repo_url)
-    return {"status": "success"}
+        doc.append("commit_history", {
+            "user": committer,
+            "commit_sha": commit_id,
+            "commit_message": message,
+            "received_time": received_time
+        })
+    doc.save(ignore_permissions=True)
+    send_telegram_message(committer, commit_id, message, received_time)
+    run_ansible_playbook(repo_url)
+    return {"status": "success", "commits_added": len(commits)}
 
 def send_telegram_message(committer, commit_id, message, received_time):
     """
