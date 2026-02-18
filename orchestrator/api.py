21f0a206d388772094cfc2d33900b9818948873f
munyaradzi <chirovemunyaradzi@gmail.com>
2026-02-18 19:32:15 +0200

    commit2

--- orchestrator/api.py
+++ orchestrator/api.py
@@ -34,8 +34,6 @@ def github_webhook(**kwargs):
         message = commit.get("message")
         received_time = now_datetime().astimezone(tz)
 
-
-
         doc.append("commit_history", {
             "user": committer,
             "commit_sha": commit_id,
