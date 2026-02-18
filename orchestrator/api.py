c43d6a53320f858d22190de1e56a2281825257c3
munyaradzi <chirovemunyaradzi@gmail.com>
2026-02-18 17:48:38 +0200

    testfield

--- orchestrator/api.py
+++ orchestrator/api.py
@@ -11,7 +11,6 @@ def github_webhook(**kwargs):
     payload = kwargs.get("payload")
     if isinstance(payload, str):
         payload = json.loads(payload)
-
     commits = payload.get("commits", [])
     tz = timezone("Africa/Harare")
     repo_url = payload.get("repository", {}).get("html_url")
