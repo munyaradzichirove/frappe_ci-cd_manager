b924b25dad6451716f6b82fd7f1284b7be6c0ef7
Munyaradzi Chirove <115703947+munyaradzichirove@users.noreply.github.com>
2026-02-21 23:43:40 +0200

    first commit

--- orchestrator/api.py
+++ orchestrator/api.py
@@ -22,7 +22,7 @@ def github_webhook(**kwargs):
     if doc_list:
         doc = frappe.get_doc("App Manager", doc_list[0].name)
     else:
-        # create parent if it doesn't exist
+        
         doc = frappe.get_doc({
             "doctype": "App Manager",
             "repo": repo_url
